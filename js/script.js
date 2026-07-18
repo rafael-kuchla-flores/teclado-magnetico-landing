33eedf2a1099943b5b3c04943e3e9079a135f42d
rafael-kuchla-flores <rafael.kuchla.flores@gmail.com>
2026-07-18 00:35:46 -0300

    feat: adiciona consumo da API ViaCEP para preenchimento automatico de endereco

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@ const btnSaibaMais = document.querySelector('#btn-saiba-mais')
 btnSaibaMais.addEventListener('click', () => {
   document.querySelector('#recursos').scrollIntoView({ behavior: 'smooth' })
 })
+
+const inputCep = document.querySelector('#cep')
+const inputEndereco = document.querySelector('#endereco')
+
+inputCep.addEventListener('blur', async () => {
+  const cep = inputCep.value.replace('-', '')
+
+  try {
+    const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    const dados = await resposta.json()
+
+    if (dados.erro) {
+      alert('CEP não encontrado')
+      return
+    }
+    inputEndereco.value = `${dados.logradouro}, ${dados.bairro} - ${dados.localidade}/${dados.uf}`
+  } catch (erro) {
+    console.error('Deu erro:', erro)
+  }
+})
